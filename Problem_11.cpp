89270234a287fef352f4b99882c65592c6280081
B.Sathvik Reddy <sathvikspartan@gmail.com>
2025-11-10 13:58:13 +0530

    Create Problem_11.cpp

---
+++ Problem_11.cpp
@@ -0,0 +1,8 @@
+// Write a Program to Check Whether a Number Is Prime or Not.
+/*
+The number can be prime or non-prime based on the number of its factors. 
+In this program, we have to check whether the given number is prime or not and print the result on the console screen.
+
+NOTE THAT - A Prime Number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+*/
+
